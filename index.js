7bc8116d287f4478b83c89524e90bb440773ad67
Eric W. Henderson <ehenderson421@gmail.com>
2022-10-03 16:29:20 +0000

    Update index.js and package-lock.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (request, response) => {
-  response.send("It worked!")
+  response.send("It worked, and my name is Eric Henderson")
 })
 
 app.listen(3000)
