25c1c85e7bb7f9ebe2af701f78a42d15903beee8
Eric W. Henderson <ehenderson421@gmail.com>
2022-10-03 20:17:11 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send("It worked, and my name is Eric");
+  response.send("It worked, and my name is Eric!");
 });
 
 app.listen(3000);
